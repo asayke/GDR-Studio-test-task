ffebfce97fbfc6c66aa9bd0cdc093ad7c4679178
oleja <noilzep@gmail.com>
2022-08-13 22:55:50 +0300

    Code Refactoring

--- Assets/Scripts/GamePlay/MapGeneration.cs
+++ Assets/Scripts/GamePlay/MapGeneration.cs
@@ -4,7 +4,9 @@ public class MapGeneration : MonoBehaviour
 {
     [SerializeField] private GameObject _coinPrefab;
     [SerializeField] private GameObject _thornPrefab;
-
+    private float minX = -8f;
+    private float minY = -5.8f;
+    
     private Camera _mainCamera;
 
     private void Awake()
@@ -18,11 +20,17 @@ public class MapGeneration : MonoBehaviour
         SpawnCoins();
     }
 
+    private bool IsSpawnNotAvalible(Vector2 point)
+    {
+        return point.x < minX && point.y < minY;
+    }
+
     private void SpawnThorns()
     {
         for (int i = 1; i <= GameParameters.MaxThornsCount; i++)
         {
             Vector2 randomPointOnScreen = _mainCamera.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+            if (IsSpawnNotAvalible(randomPointOnScreen)) i--;
             Instantiate(_thornPrefab, randomPointOnScreen, Quaternion.identity);
         }
     }
@@ -32,6 +40,7 @@ public class MapGeneration : MonoBehaviour
         for (int i = 1; i <= GameParameters.MaxCoinsCount; i++)
         {
             Vector2 randomPointOnScreen = _mainCamera.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+            if (IsSpawnNotAvalible(randomPointOnScreen)) i--;
             Instantiate(_coinPrefab, randomPointOnScreen, Quaternion.identity);
         }
     }
